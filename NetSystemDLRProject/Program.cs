d8f84fb340ebc808587d1b964f8aea4a70261856
Maxim Efimov <efimov.top.academy@gmail.com>
2025-12-09 08:34:13 +0300

    09-12-25

--- NetSystemDLRProject/Program.cs
+++ NetSystemDLRProject/Program.cs
@@ -7,5 +7,22 @@ scriptEngine.Execute("print('Hello world')");
 scriptEngine.ExecuteFile("prog01.py");
 
 
+int x = 23, y = 45;
+ScriptScope scriptScope = scriptEngine.CreateScope();
+scriptScope.SetVariable("x", x);
+scriptScope.SetVariable("y", y);
+
+scriptEngine.ExecuteFile("prog02.py", scriptScope);
+dynamic z = scriptScope.GetVariable("z");
+Console.WriteLine($"From NET: z = {z}");
+
+float a = 2f;
+int exp = 5;
+
+dynamic binPower = scriptScope.GetVariable("binPower");
+dynamic result = binPower(a, exp);
+Console.WriteLine(result);
+
+
 
 
---
+++ NetSystemDLRProject/prog02.py
@@ -0,0 +1,11 @@
+﻿def binPower(x, n):
+	if n == 0:
+		return 1
+	h = binPower(x, n // 2)
+	h *= h
+	if n % 2 == 1:
+		h *= x
+	return h
+
+z = x + y
+print("z =", z)
